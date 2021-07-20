81276fbe9fa3135b2955a2a43877ffe2a27e6f33
dayojiboye <dayojiboye2@gmail.com>
2021-07-20 03:43:30 +0100

    created the header component

--- src/components/ui/header/index.js
+++ src/components/ui/header/index.js
@@ -1,9 +1,33 @@
 import React from "react";
 
+import { ReactComponent as IdeaIcon } from "../../../assets/images/idea.svg";
+import Select from "../select";
 import "./styles.scss";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="header">
+      <div className="suggestions">
+        <IdeaIcon />
+        
+        <span>6 Suggestions</span>
+      </div>
+
+      <div className="sort">
+        <span>Sort by:</span>
+
+        <Select>
+          <option>Most Upvotes</option>
+        </Select>
+      </div>
+
+      <div className="add-feedback">
+        <button>
+        <i class="fas fa-plus"></i> Add Feedback
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
